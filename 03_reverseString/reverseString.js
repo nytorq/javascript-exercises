58358b13beb13c8c55917abc92d8b591aeed4c06
nytorq <7298152+nytorq@users.noreply.github.com>
2024-03-15 09:30:42 -0600

    Solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedArray = [];
+    let reversedString = '';
+    let splitString = str.split("");
+    let stringLength = splitString.length;
+    for (i = 0 ; i < stringLength ; i++) {
+        let char = splitString.pop();
+        reversedArray.push(char);
+        reversedString = reversedArray.join("");
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
