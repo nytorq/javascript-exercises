5775470db8f427b60914eaa8934a826d22562adf
nytorq <7298152+nytorq@users.noreply.github.com>
2024-03-15 08:44:25 -0600

    solution for repeat.string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    if (num > 0) {
+        for (i = 0 ; i < num ; i++) {
+            output = output.concat(string);
+        }
+        return output;
+    } else if (num < 0) {
+        return "ERROR";
+    } else if (num === 0) {
+        return ""
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
