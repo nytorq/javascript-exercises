98a40990ef4afb69641ad3e2302750d798e153a4
nytorq <7298152+nytorq@users.noreply.github.com>
2024-03-25 13:35:15 -0600

    Working on solution for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(val1, val2) {
+    let result = 0;
+    if (val1 < 0 || 
+        val2 < 0 || 
+        typeof val1 === 'string' || 
+        typeof val2 === 'string' ||
+        Array.isArray(val1) || 
+        Array.isArray(val2)
+        ) {
+        result = "ERROR";
+    } else {
+        // for (i = val1 ; i <= val2 ; i++) {
+        //     result += val1;
+        // }
+        /* PSEUDOCODE
+            - Determine which of the values is larger and Makes that the stopping point for the loop
+            - Make result equal to smaller value
+            - For loop:
+                - Start i at smaller value    
+                - Determine how many iterations to do  (larger val - smaller val + 1)
+                - Store current value as i
+                - 
+        */
+    } 
+    return result
 };
 
 // Do not edit below this line
