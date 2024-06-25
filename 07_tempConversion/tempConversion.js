a306ccec98e4e83acd3741e85f174302fba8a16b
nytorq <7298152+nytorq@users.noreply.github.com>
2024-06-25 17:55:22 -0600

    Solution found

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(value) {
+  value = (value * 9 / 5) + 32;
+  value = value.toFixed(1);
+  value = parseFloat(value);
+  return value;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(value) {
+  value = (value - 32) *5/9;
+  value = value.toFixed(1);
+  value = parseFloat(value);
+  return value;
 };
 
 // Do not edit below this line
