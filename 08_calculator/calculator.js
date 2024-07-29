558b100771ed2f7dbdded58b19634ac8b2b397b6
nytorq <charlieprator@gmail.com>
2024-07-29 15:50:21 -0600

    First attempt

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let accumulator = 0;
+	for (i = 0; i<x.length ; i++) {
+    accumulator += x[i];
+  }
+  return accumulator;
 };
 
-const multiply = function() {
-
+const multiply = function(a) { 
+  a.reduce((accumulator, currentValue)=>{
+    accumulator *= currentValue;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  // if (x === 0) {
+  //   return 1;
+  // } else {
+  //   let accumulator = x;>>>>
+  //   for (i=x ; i > 0 ; i--) {
+  //     accumulator *= x[i];
+  //   }
+  //   return accumulator;
+  // }
 };
 
 // Do not edit below this line
