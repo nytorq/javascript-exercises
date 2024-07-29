283f5902d9f82c10c8c32bc594b86dda02469c43
nytorq <charlieprator@gmail.com>
2024-07-29 17:03:05 -0600

    Finished solution for 08_Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,26 +14,29 @@ const sum = function(x) {
   return accumulator;
 };
 
-const multiply = function(a) { 
-  a.reduce((accumulator, currentValue)=>{
-    accumulator *= currentValue;
-  }, 1);
-};
+// const multiply = function(a) { 
+//   a.reduce((accumulator, currentValue)=>{
+//     return accumulator *= currentValue;
+//   }, 1);
+// };
+
+const multiply = function(a) {return a.reduce((accumulator, currentValue)=> accumulator *= currentValue,1);}
+
 
 const power = function(x, y) {
 	return x ** y;
 };
 
 const factorial = function(x) {
-  // if (x === 0) {
-  //   return 1;
-  // } else {
-  //   let accumulator = x;>>>>
-  //   for (i=x ; i > 0 ; i--) {
-  //     accumulator *= x[i];
-  //   }
-  //   return accumulator;
-  // }
+  if (x === 0) {
+    return 1;
+  } else {
+    let newArray = [];
+    for (i=x ; i > 0 ; i-- ) {
+      newArray.push(i);
+    }
+    return newArray.reduce((accumulator, currentValue) => accumulator *= currentValue ,1);
+  }
 };
 
 // Do not edit below this line
